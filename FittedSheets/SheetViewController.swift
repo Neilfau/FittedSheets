6ad065c6562b2d0cc573ce6000c90797317dfcc8
Neil Faulkner <neilfau@icloud.com>
2021-12-01 11:44:52 +0100

    Revert "wip"
    This reverts commit 53431a0d52543748cd0f156ac16951dc801da720.

--- FittedSheets/SheetViewController.swift
+++ FittedSheets/SheetViewController.swift
@@ -471,7 +471,6 @@ public class SheetViewController: UIViewController {
                     self.isPanning = false
                     if previousSize != newSize {
                         self.sizeChanged?(self, newSize, newContentHeight)
-                        self.didPanSheet?(self, newHeight, gesture.state)
                     }
                 })
             case .possible:
